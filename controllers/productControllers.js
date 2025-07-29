ec891f1925d1a917aff011822c285cb7fe7f1fdc
Githusigan2002 <jeyarupangithusigan@gmail.com>
2025-07-29 23:24:49 +0530

    Add getOneProduct controller and update product routes

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -3,7 +3,6 @@ import Products from "../models/Product.model.js";
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Products.find();
-    if (!products) return res.json("Products Empty");
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({
@@ -26,5 +25,21 @@ export const addNewProduct = async (req, res) => {
     res.status(201).json({ message: "Product added Sucessfull!" });
   } catch (error) {
     console.log("Failed to add product", error.message);
+  }
+};
+
+export const getOneProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Products.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { addNewProduct, getAllProducts } from "../controllers/productControllers.js";
+import { addNewProduct, getAllProducts,getOneProduct } from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.get("/all-products", getAllProducts);
-// router.get("/api/products/:id", getOneProducts);
+router.get("/:id", getOneProduct);
 router.post("/add-products",addNewProduct);
 // router.put("/api/products/:id", getOneProducts);
 // router.delete("/api/products/:id", getOneProducts);
