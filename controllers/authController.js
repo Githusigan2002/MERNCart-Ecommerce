4dd6680ed446f2c2a35f265e30d7f82c66808638
Githusigan2002 <jeyarupangithusigan@gmail.com>
2025-07-29 21:21:28 +0530

    Implement JWT authentication for user login and update routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,35 @@
 import User from "../models/User.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+
+export const loginUser = async (req,res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched)
+      return res.status(404).json({ message: "Invalid password" });
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
 
 export const registerUser = async (req, res) => {
   try {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser,loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.get("/register", registerUser);
-// router.post("/login", loginUser);
+router.get("/api/auth/register", registerUser);
+router.post("/api/auth/login", loginUser);
 
 export default router;
