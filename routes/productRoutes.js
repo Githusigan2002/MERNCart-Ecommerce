1a342c3368bb8369bab62c11124eadc250d832c3
Githusigan2002 <jeyarupangithusigan@gmail.com>
2025-08-01 20:50:41 +0530

    Implement auth middleware protect routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,11 +7,14 @@ import {
   deleteProduct,
 } from "../controllers/productControllers.js";
 
+import { authMiddleware, isAdmin } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
 router.get("/all-products", getAllProducts);
 router.get("/:id", getOneProduct);
-router.post("/add-products", addNewProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+
+router.post("/add-products", authMiddleware, isAdmin, addNewProduct);
+router.put("/:id", authMiddleware, updateProduct);
+router.delete("/:id", authMiddleware, deleteProduct);
 export default router;
